05af7f345afcf670bc68c6122b8dc71af45739f0
Nikola Pavlovic <pavlovicn410@gmail.com>
2022-04-13 12:23:54 +0200

    api setup

--- js/index.js
+++ js/index.js
@@ -1,12 +1,33 @@
 
+const apiKey = "8ee7f4d4c46b1cba684adc6118411654"
+
 function getMovies() {
     axios
-      .get("https://api.themoviedb.org/3/movie/550?api_key=8ee7f4d4c46b1cba684adc6118411654")
+      .get(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&query=all`)
     .then(function (response) {
       console.log(response.data);
     });
 }
 
+function searchMovies(movieTitle) {
+  axios
+    .get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieTitle}`)
+  .then(function (response) {
+    console.log(response.data);
+  });
+}
+
+function getMovieById(id) {
+  axios
+  .get(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
+.then(function (response) {
+  console.log(response.data);
+});
+  
+}
+
 getMovies()
 
-// KEY =8ee7f4d4c46b1cba684adc6118411654
+getMovieById(634649)
+
+searchMovies("jaws")
